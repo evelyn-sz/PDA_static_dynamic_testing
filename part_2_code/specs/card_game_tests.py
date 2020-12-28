fc1b4e56eae6cc2804f59d86812ec670b27544f6
Evelyn Szarek <evelynszarek@gmail.com>
2020-12-28 18:11:28 +0000

    all tests passed

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,20 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.club = Card("club", 1)
+        self.diamond = Card("diamond", 2)
+        self.heart = Card("heart", 3)
+        self.spade = Card("spade", 4)
+        self.card_game = CardGame()
+        self.cards = [self.club, self.diamond, self.heart, self.spade]
+
+    def test_card_has_ace_equal_1(self):
+        self.assertEqual(True, self.card_game.check_for_ace(self.club))
+
+    def test_highest_card(self):
+        self.assertEqual(self.diamond, self.card_game.highest_card(self.diamond, self.club))
+
+    def test_cards_total(self):
+        self.assertEqual("You have a total of 10", self.card_game.cards_total(self.cards))
